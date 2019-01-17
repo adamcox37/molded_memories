644e08d2b81448b9a7c6ca650a61d976c33adc5c
Adam Cox <adam_mcox@yahoo.com>
2019-01-17 06:13:36 -0500

    add curtain menu, fix javascript link

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,9 @@
-// Nav dropdown //
+// Menu Options //
 
-// When the user clicks on the button, toggle between hiding and showing the dropdown content
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick= function(event) {
-	if (!event.target.matches('.dropbtn')) {
-
-		var dropdowns= document.getElementById("dropdown-content");
-		var i;
-		for (i= 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
-
-// Pricing //
-
-
+function openNav() {
+	document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+  }
